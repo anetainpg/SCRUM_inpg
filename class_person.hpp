c9283e015ff416ca599b970d2855e0791fa729e7
Aneta <awalas.inpg@gmail.com>
2019-05-10 15:51:24 +0200

    Klasa wersja.2.1
    Poprawa klasy Person, która posiada tylko imię, nazwisko oraz pesel. Należy stworzyć klasę dziedziczącą zawierajacą pozostałe pola. Należy dodać destruktor do klasy Person.

--- class_person.hpp
+++ class_person.hpp
@@ -1,43 +1,25 @@
+#ifndef CLASS_PERSON_HPP
+#define CLASS_PERSON_HPP
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 
-class Osoba{
-public:
-    //potrzebny konstruktor
-    void set_name(){};
-    void set_surname() {};
-    void set_town() {};
-    void set_street() {};
-    void set_phone() {};
-    void set_pesel() {};
-    void set_eye() {};
-    void set_birth() {};
-    void set_height() {};
-    void set_kids() {};
-    void set_wage() {};
-
-    string get_name()const{};
-    string get_surname()const{};
-    string get_town()const{};
-    string get_street()const{};
-    int get_phone()const{};
-    int get_pesel()const{};
-    string get_eye()const{};
-    string get_birth()const{};
-    int get_height()const{};
-    int get_kids()const{};
-    int get_wage()const{};
+class Person{
 private:
-    string name_;
-    string surname_;
-    string town_;
-    string street_;
-    int phone_num_;
+    std::string name_;
+    std::string surname_;
+    std::vector<std::string> vect_name_sur_{name_,surname_};
     int pesel_num_;
-    string eye_color_;
-    string date_birth_;
-    int height_;
-    int num_kids_;
-    int wage_;
+public:
+    Person(std::vector<std::string> vect_name_sur, int pesel_num) :
+           vect_name_sur_(vect_name_sur), pesel_num_(pesel_num) {}
+
+    std::string get_name() const { return name_; }
+    std::string get_surname() const { return surname_; }
+    int get_pesel() const { return pesel_num_; }
+
+    //należy napisać funkcję tworzącą parę klucz(pesel) oraz wartość(imię i nazwisko)
+
 };
+#endif
